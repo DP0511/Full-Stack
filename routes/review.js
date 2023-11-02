c599bf32a4b24d5f46eab8f459420ee39cceaf7d
DP0511 <pvt.dhairyaprajapati@gmail.com>
2023-11-02 20:24:42 +0530

    Add Project Files

---
+++ routes/review.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const router = express.Router({ mergeParams: true });
+const wrapAsync = require("../utils/wrapAsync.js");
+const { isLoggedIn, isAuthor, validateReview } = require("../middleware.js");
+const reviewController = require("../controllers/review.js");
+
+//Post Review Route
+router.post(
+  "/",
+  isLoggedIn,
+  validateReview,
+  wrapAsync(reviewController.createReview)
+);
+
+//Delete Review Route
+router.delete(
+  "/:reviewId",
+  isLoggedIn,
+  isAuthor,
+  wrapAsync(reviewController.deleteReview)
+);
+
+module.exports = router;
